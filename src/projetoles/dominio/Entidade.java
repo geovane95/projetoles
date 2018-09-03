0dcd2511079b5f88ba54172c893bdb99a52fabf9
Geovane Jr <geovane.a.f.junior@gmail.com>
2018-09-03 13:42:27 -0300

    Commit páginas de livro.

---
+++ src/projetoles/dominio/Entidade.java
@@ -0,0 +1,4 @@
+package projetoles.dominio;
+
+public interface Entidade {
+}
---
+++ src/projetoles/dominio/EntidadeDominio.java
@@ -0,0 +1,13 @@
+package projetoles.dominio;
+
+public class EntidadeDominio implements Entidade{
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
